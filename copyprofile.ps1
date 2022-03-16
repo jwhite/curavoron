b25455ccacb79d2e727acf020a9cb0354c36c109
Jeremy White <sail.madeline@gmail.com>
2022-03-17 10:01:27 +1300

    Added 0.25 and .4 profiles for Dragon SF

--- copyprofile.ps1
+++ copyprofile.ps1
@@ -2,3 +2,5 @@ $curapath = "C:\\Program Files\\Ultimaker Cura 4.11.0\\resources"
 
 Copy-Item -Path variants\\voron2_350_dragon_0.25.inst.cfg -Destination (Join-Path $curapath "variants")
 Copy-Item -Path variants\\voron2_350_dragon_0.40.inst.cfg -Destination (Join-Path $curapath "variants")
+
+Copy-Item -Path "quality\\voron2\\*" -Destination (Join-Path $curapath "quality\\voron2")
